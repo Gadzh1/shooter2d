0298c666322bbfe9b4a3be7b38cfe269b6d4b26d
Gadzh1 <musievgadzi64@gmail.com>
2023-12-22 19:47:48 +0300

    ref

--- game/constants.py
+++ game/constants.py
@@ -1,6 +1,4 @@
 import os
-import pygame
-
 
 WIDTH = 400
 HEIGHT = 600
@@ -8,7 +6,6 @@ FPS = 60
 LASER_PRICE = 5
 GAME_CHANCES = 3
 SHOOT_DELAY = 250
-LAST_SHOT = pygame.time.get_ticks()
 
 GAME_FOLDER = os.path.dirname(__file__)
 IMG_FOLDER = os.path.join(GAME_FOLDER, 'img')
--- game/shooter.py
+++ game/shooter.py
@@ -1,3 +1,4 @@
+import pygame
 import random
 
 from constants import *
@@ -8,12 +9,6 @@ from explosion import Explosion
 from health import Health
 
 
-pygame.init()
-display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('test')
-clock = pygame.time.Clock()
-
-
 def create_bullet(btype, player, shoot_sounds, all_sprites, bullets):
     bullet = Bullet(player.rect.centerx, player.rect.top, btype)
     random.choice(shoot_sounds).play()
@@ -22,10 +17,10 @@ def create_bullet(btype, player, shoot_sounds, all_sprites, bullets):
 
 
 def shoot():
-    global LAST_SHOT
+    global last_shot
     now = pygame.time.get_ticks()
-    if now - LAST_SHOT > SHOOT_DELAY:
-        LAST_SHOT = now
+    if now - last_shot > SHOOT_DELAY:
+        last_shot = now
         create_bullet('blue', player, shoot_sounds, all_sprites, bullets)
 
 
@@ -40,9 +35,6 @@ def draw_bar(surf, image, bar_healths):
     offset_x = part_width * (bar_healths - 1)
 
     surf.blit(image, (230, 10), (offset_x, 0, part_width, image.get_height()))
-
-
-font_name = pygame.font.match_font('arial')
 
 
 def draw_text(text, size, surf, x, y):
@@ -62,24 +54,42 @@ def draw_capabilities():
         display.blit(green_ball, green_rect)
 
 
+def check_keys_state():
+    global score
+    keys_state = pygame.key.get_pressed()
+    if keys_state[pygame.K_s]:
+        shoot()
+    if keys_state[pygame.K_w]:
+        if score >= LASER_PRICE:
+            score -= LASER_PRICE
+            create_bullet('red', player, shoot_sounds, all_sprites, bullets)
+
+
+last_shot = pygame.time.get_ticks()
+
+pygame.init()
+display = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('shooter')
+clock = pygame.time.Clock()
+
+font_name = pygame.font.match_font('arial')
+
 chances = pygame.sprite.Group()
 all_sprites = pygame.sprite.Group()
 mobs = pygame.sprite.Group()
 bullets = pygame.sprite.Group()
 
-game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, 'img')
-snd_dir = os.path.join(game_folder, 'snd')
+snd_dir = os.path.join(GAME_FOLDER, 'snd')
 
-background = pygame.image.load(os.path.join(img_folder, 'blue.png')).convert()
+background = pygame.image.load(os.path.join(IMG_FOLDER, 'blue.png')).convert()
 
-shield_img = pygame.image.load(os.path.join(img_folder, 'sp_bar_health_strip12.png')).convert()
+shield_img = pygame.image.load(os.path.join(IMG_FOLDER, 'sp_bar_health_strip12.png')).convert()
 shield_img.set_colorkey((0, 0, 0))
 size = shield_img.get_size()
 shield_img = pygame.transform.scale(shield_img, (size[0] * 2.5, size[1] * 2.5))
 
-green_surf = pygame.image.load(os.path.join(img_folder, 'green.png')).convert()
-red_surf = pygame.image.load(os.path.join(img_folder, 'red.png')).convert()
+green_surf = pygame.image.load(os.path.join(IMG_FOLDER, 'green.png')).convert()
+red_surf = pygame.image.load(os.path.join(IMG_FOLDER, 'red.png')).convert()
 
 red_ball = pygame.transform.scale(red_surf, (70, 70))
 red_rect = red_surf.get_rect()
@@ -123,7 +133,6 @@ player = Player()
 all_sprites.add(player)
 
 score = 0
-
 switch = True
 
 while switch:
@@ -132,14 +141,7 @@ while switch:
         if event.type == pygame.QUIT:
             switch = False
 
-    keys_state = pygame.key.get_pressed()
-
-    if keys_state[pygame.K_s]:
-        shoot()
-    if keys_state[pygame.K_w]:
-        if score >= LASER_PRICE:
-            score -= LASER_PRICE
-            create_bullet('red', player, shoot_sounds, all_sprites, bullets)
+    check_keys_state()
 
     for i in mobs:
         if i.rect.top > HEIGHT or i.rect.right < 0 or i.rect.left > WIDTH:
